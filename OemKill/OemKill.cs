9a62c26da63f05c2f7ef1110c9a745f2c0ced7b3
X9VoiD <oscar.silvestrexx@gmail.com>
2020-07-26 14:07:13 +0800

    Add OemKill

---
+++ OemKill/OemKill.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using TabletDriverPlugin;
+using TabletDriverPlugin.Attributes;
+
+namespace OemKill
+{
+    [SupportedPlatform(PluginPlatform.Windows)]
+    [PluginName("OEM Kill")]
+    public class OemKillTool : ITool
+    {
+        public bool Initialize()
+        {
+            var processList = Process.GetProcesses();
+            var oemProcesses = from process in processList
+                               where OemProcesses.Contains(process.ProcessName)
+                               select process;
+
+            if (oemProcesses.ToList().Count == 0)
+            {
+                Log.Write("OemKill", "No processes found");
+                return true;
+            }
+
+            try
+            {
+                foreach (var process in oemProcesses)
+                    process.Kill();
+            }
+            catch (Exception e)
+            {
+                Log.Write("OemKill", e.Message, true);
+                return false;
+            }
+
+            Log.Write("OemKill", "Oem process killed successfully");
+            return true;
+        }
+
+        List<string> OemProcesses = new List<string>
+        {
+            // XP-Pen
+            "PentabletService",
+            "PenTablet"
+        };
+
+        public void Dispose() { }
+    }
+}
