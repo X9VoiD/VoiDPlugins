c5c5f2a6b0d9c0fa8dbdffe0545b177aef223245
Valentin Boettcher <hiro@protagon.space>
2024-01-27 18:20:09 -0500

    implement relative precision mode

--- src/Filter/PrecisionControl/PrecisionControl.cs
+++ src/Filter/PrecisionControl/PrecisionControl.cs
@@ -4,6 +4,7 @@ using OpenTabletDriver.Plugin;
 using OpenTabletDriver.Plugin.Attributes;
 using OpenTabletDriver.Plugin.Output;
 using OpenTabletDriver.Plugin.Tablet;
+using OpenTabletDriver.Plugin.Timing;
 
 namespace VoiDPlugins.Filter
 {
@@ -12,12 +13,17 @@ namespace VoiDPlugins.Filter
     {
         internal static Vector2 StartingPoint;
         internal static bool IsActive { set; get; }
+        internal static bool IsRelative { set; get; }
         internal static bool SetPosition { set; get; }
 
         public static string[] ValidModes => new[] { "Toggle", "Hold" };
+        public static string[] ValidRelativeModes => new[] { "Absolute", "Relative" };
 
         [Property("Mode"), PropertyValidated(nameof(ValidModes))]
         public string? Mode { set; get; }
+
+        [Property("Relative Mode"), PropertyValidated(nameof(ValidRelativeModes))]
+        public string? RelativeMode { set; get; }
 
         public void Press(TabletReference tablet, IDeviceReport report)
         {
@@ -25,6 +31,9 @@ namespace VoiDPlugins.Filter
                 IsActive = !IsActive;
             else if (Mode == "Hold")
                 IsActive = true;
+
+            IsRelative = (RelativeMode == "Relative");
+
 
             SetPosition = true;
         }
@@ -41,10 +50,23 @@ namespace VoiDPlugins.Filter
     {
         public event Action<IDeviceReport>? Emit;
 
+        private readonly HPETDeltaStopwatch _stopwatch = new HPETDeltaStopwatch();
+
         [SliderProperty("Precision Multiplier", 0.0f, 10f, 0.3f), DefaultPropertyValue(0.3f)]
         public float Scale { get; set; }
 
+        [SliderProperty("Timeout", 100f, 1000f, 1f), DefaultPropertyValue(50), Unit("ms")]
+        [ToolTip("Time period after which the position is reset in relative mode.")]
+        public double Timeout
+        {
+            get { return timeout.TotalMilliseconds; }
+            set { timeout = TimeSpan.FromMilliseconds(value); }
+        }
+        private TimeSpan timeout;
+
         public PipelinePosition Position => PipelinePosition.PostTransform;
+        internal static Vector2 LastPosition;
+        internal static Vector2 NewZero;
 
         public void Consume(IDeviceReport value)
         {
@@ -53,14 +75,29 @@ namespace VoiDPlugins.Filter
                 if (PrecisionControlBinding.SetPosition)
                 {
                     PrecisionControlBinding.StartingPoint = report.Position;
+                    NewZero = report.Position;
                     PrecisionControlBinding.SetPosition = false;
+                }
+
+                if (PrecisionControlBinding.IsRelative)
+                {
+                    var deltaTime = _stopwatch.Restart();
+
+                    if (deltaTime > timeout) {
+                        PrecisionControlBinding.StartingPoint = LastPosition;
+                        NewZero = report.Position;
+                    }
                 }
 
                 if (PrecisionControlBinding.IsActive)
                 {
-                    var delta = (report.Position - PrecisionControlBinding.StartingPoint) * Scale;
+                    var relativeTo = PrecisionControlBinding.IsRelative ? NewZero : PrecisionControlBinding.StartingPoint;
+                    var delta = (report.Position - relativeTo) * Scale;
                     report.Position = PrecisionControlBinding.StartingPoint + delta;
                 }
+
+                LastPosition = report.Position;
+
                 value = report;
             }
 
